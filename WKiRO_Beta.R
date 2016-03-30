37c6816f588c6de96951c6933c1f03817c3d89b6
Your Name <you@example.com>
2016-03-30 16:12:26 +0200

    koncowe zmany

--- WKiRO_Beta.R
+++ WKiRO_Beta.R
@@ -1,9 +1,9 @@
 library(mlr)
 library(pROC)
 source("functions.R")
-
+for(i in 0:9){
 DATA_DIR <- "data"
-IRIS_DATA_FILE_NAME <- "glass.dataWithNames"
+IRIS_DATA_FILE_NAME <- "optdigits.dataWithNames"
 IRIS_CLASS_COLLUMN_NAME <- "class"
 CLASSIF_LDA <- "classif.lda"
 CLASSIF_SVM <- "classif.svm"
@@ -16,28 +16,10 @@ data <- getData(IRIS_DATA_FILE_NAME)
 task = makeClassifTask(data = data$trainData, target = "class")
 
 
-lrnSvn = makeLearner(CLASSIF_SVM, predict.type = PREDICT_TYPE, kernel = "sigmoid")
-lrnLda = makeLearner(CLASSIF_LDA, predict.type = PREDICT_TYPE )
-lrnKnn = makeLearner(CLASSIF_knn)
-lrnNaiveBayes = makeLearner(CLASSIF_LDA, predict.type = PREDICT_TYPE)
-
-
-modelSvn = train(lrnSvn, task)
-modelLda = train(lrnLda, task)
-modelNaiveBayes = train(lrnNaiveBayes, task)
-modelKnn = train(lrnKnn, task)
-
-wynik = getMAndCi(trainAndPredict(lrnSvn,task,data$testData),data$testData$class)
-print(wynik$M)
-print(wynik$Ci)
-wynik = getMAndCi(trainAndPredict(lrnLda,task,data$testData),data$testData$class)
-print(wynik$M)
-print(wynik$Ci)
-wynik = getMAndCi(trainAndPredict(lrnNaiveBayes,task,data$testData),data$testData$class)
-print(wynik$M)
-print(wynik$Ci)
-wynik = wynik = getMAndCi(trainAndPredict(lrnKnn,task,data$testData),data$testData$class)
-print(wynik$M)
-print(wynik$Ci)
+knnClassif(task,data$testData)
+ldaClassif(task,data$testData)
+svmClassif(task,data$testData)
+naiveBayesClassif(task,data$testData)
+}
 
 
--- functions.R
+++ functions.R
@@ -17,9 +17,9 @@ getData <- function(FileName){
 
 getMAndCi <- function(predictions,target){
   predictionsResponse <- as.numeric(getPredictionResponse(predictions))
-  multiclassResult = multiclass.roc(as.numeric(target), predictionsResponse)
+  multiclassResult = multiclass.roc(target, predictionsResponse)
   aucResult = auc(multiclassResult)
-  ci = ci(predictionsResponse, as.numeric(target))
+  ci = ci.auc(target,predictionsResponse)
   return(list("M" = aucResult, "Ci" = ci))
 }
 
@@ -28,3 +28,52 @@ trainAndPredict <-function(lrn, task, data){
   predictions = predict(model, newdata = data)
   return(predictions)
 }
+knnClassif <- function(task,testData){
+  print("KNN****************************")
+  lrnKnn = makeLearner(CLASSIF_knn, k = 9)
+  wynik = wynik = getMAndCi(trainAndPredict(lrnKnn,task,testData),testData$class)
+  print(wynik$M)
+  print(wynik$Ci)
+  print("--------------")
+}
+ldaClassif <- function(task,testData){
+  print("LDA****************************")
+  lrnLda = makeLearner(CLASSIF_LDA, predict.type = PREDICT_TYPE)
+  wynik = wynik = getMAndCi(trainAndPredict(lrnLda,task,testData),testData$class)
+  print(wynik$M)
+  print(wynik$Ci)
+  print("--------------")
+}
+
+svmClassif <- function(task,testData){
+  print("SVM****************************")
+  lrnSvn = makeLearner(CLASSIF_SVM, predict.type = PREDICT_TYPE, kernel = "sigmoid")
+  wynik = wynik = getMAndCi(trainAndPredict(lrnSvn,task,testData),testData$class)
+  print(wynik$M)
+  print(wynik$Ci)
+  print("--------------")
+}
+naiveBayesClassif <- function(task,testData){
+  print("NaiveBayes****************************")
+  lrnNaiveBayes = makeLearner(CLASSIF_naiveBayes)
+  wynik = wynik = getMAndCi(trainAndPredict(lrnNaiveBayes,task,testData),testData$class)
+  print(wynik$M)
+  print(wynik$Ci)
+  print("--------------")
+}
+
+
+# knnClassif <- function(task,testData){
+#   print("KNN****************************")
+#   for (i in 1:50){
+#     if((i %% 6) == 0){
+#       lrnKnn = makeLearner(CLASSIF_knn, k = i)
+#       wynik = wynik = getMAndCi(trainAndPredict(lrnKnn,task,testData),testData$class)
+#       print (c("knn k = ",i))
+#       print(wynik$M)
+#       print(wynik$Ci)
+#       print("--------------")
+#     }
+#     
+#   }
+# }
